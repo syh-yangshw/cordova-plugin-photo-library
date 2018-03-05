00e52f94a63fb4d60df8fc07463375ce237e9aca
yangshiwei <yangshiwei@syhvip.com>
2018-03-05 10:52:53 +0800

    修改默认的下载图片文件名称,加入时间戳以及随机数

--- src/android/PhotoLibraryService.java
+++ src/android/PhotoLibraryService.java
@@ -554,9 +554,14 @@ public class PhotoLibraryService {
 
   private File getImageFileName(File albumDirectory, String extension) {
     Calendar calendar = Calendar.getInstance();
+    int theMonth = calendar.get(Calendar.MONTH) + 1;
     String dateStr = calendar.get(Calendar.YEAR) +
-      "-" + calendar.get(Calendar.MONTH) +
-      "-" + calendar.get(Calendar.DAY_OF_MONTH);
+      "-" + String.valueOf(theMonth) +
+      "-" + calendar.get(Calendar.DAY_OF_MONTH) +
+      "-" + calendar.get(Calendar.HOUR) +
+      "-" + calendar.get(Calendar.MINUTE) +
+      "-" + calendar.get(Calendar.SECOND) +
+      Math.random();
     int i = 1;
     File result;
     do {
